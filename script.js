3b38b96df831e5c46dcd8c8aadb4c553e3658adc
snappyjel <leviftr1@gmail.com>
2023-06-03 22:07:23 +0700

    Create 16x16 grid with JS

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const container = document.querySelector(".container");
 const header = document.querySelector("header");
 
-//Create div element with grid class
-const grid = document.createElement("div");
-grid.classList.add("grid");
-container.appendChild(grid);
+//Create h1 element inside header
+const h1 = document.createElement("h1");
+h1.textContent = "Etch a Sketch";
+header.appendChild(h1);
+
+//Create 16x16 div element with grid class
+const gridCanvas = 16 * 16;
+
+for (let i = 0; i < gridCanvas; i++) {
+  const grid = document.createElement("div");
+  grid.classList.add("grid");
+  container.appendChild(grid);
+}
