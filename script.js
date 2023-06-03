722f93b3fe90c523f89d84f58697b53d9a7333fc
snappyjel <leviftr1@gmail.com>
2023-06-03 21:45:07 +0700

    Create grid dynamically with JS

---
+++ script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector(".container");
+const header = document.querySelector("header");
+
+//Create div element with grid class
+const grid = document.createElement("div");
+grid.classList.add("grid");
+container.appendChild(grid);
