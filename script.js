0163ea309b5d6aa4e7053aa9c0dace3059a96476
snappyjel <leviftr1@gmail.com>
2023-06-03 22:55:29 +0700

    Wrap grid creation in a function

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ header.appendChild(h1);
 //Create 16x16 div element with grid class
 const gridCanvas = 16 * 16;
 for (let i = 0; i < gridCanvas; i++) {
+  createGrid();
+}
+
+function createGrid() {
   const grid = document.createElement("div");
   grid.classList.add("grid");
   container.appendChild(grid);
