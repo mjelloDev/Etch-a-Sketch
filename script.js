3093995bb895dd4b0f1487416d69d51899255cc9
snappyjel <leviftr1@gmail.com>
2023-06-04 20:22:41 +0700

    Comment out condition if clicked

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const header = document.querySelector("header");
 const container = document.querySelector(".flex-container");
 const canvas = document.querySelector(".canvas");
-let isMouseDown = false;
+// let isMouseDown = false;
 
 function createGrid() {
   //Determine the size of the canvas
@@ -27,13 +27,13 @@ function createGrid() {
     for (let j = 0; j < gridSize; j++) {
       let gridCell = document.createElement("div");
       gridCell.classList.add("cell");
-      // Add event listeners for mouse clicked/not
-      gridCell.addEventListener("mousedown", () => {
-        isMouseDown = true;
-      });
-      gridCell.addEventListener("mouseup", () => {
-        isMouseDown = false;
-      });
+      // // Add event listeners for mouse clicked/not
+      // gridCell.addEventListener("mousedown", () => {
+      //   isMouseDown = true;
+      // });
+      // gridCell.addEventListener("mouseup", () => {
+      //   isMouseDown = false;
+      // });
       gridCell.addEventListener("mouseover", colorChoice);
       gridContainer.appendChild(gridCell);
     }
@@ -46,14 +46,23 @@ function resetGrid() {
 }
 
 // onclick function to choose color
+// let color = "black";
+// function colorChoice() {
+//   if (isMouseDown) {
+//     if (color === "random") {
+//       this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+//     } else {
+//       this.style.backgroundColor = color;
+//     }
+//   }
+// }
+
 let color = "black";
 function colorChoice() {
-  if (isMouseDown) {
-    if (color === "random") {
-      this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-    } else {
-      this.style.backgroundColor = color;
-    }
+  if (color === "random") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+    this.style.backgroundColor = color;
   }
 }
 
