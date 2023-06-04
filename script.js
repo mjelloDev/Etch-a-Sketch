d64ba9936c72b8c63b4407bf391ca23613c98a1f
snappyjel <leviftr1@gmail.com>
2023-06-04 19:36:33 +0700

    Add some styling to center the content

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const header = document.querySelector("header");
 const container = document.querySelector(".flex-container");
 const canvas = document.querySelector(".canvas");
+let isMouseDown = false;
 
 function createGrid() {
   //Determine the size of the canvas
   let gridSize = parseInt(document.getElementById("gridSizeInput").value);
 
   //Validate gridSize input value
-  if (gridSize < 2 || gridSize > 100) {
-    alert("Please enter between 2 and 100");
+  if (gridSize < 1 || gridSize > 100) {
+    alert("Please enter between 1 and 100");
     return; // ==> to exit the function if the number is not within the specified range
   }
 
@@ -26,6 +27,13 @@ function createGrid() {
     for (let j = 0; j < gridSize; j++) {
       let gridCell = document.createElement("div");
       gridCell.classList.add("cell");
+      // Add event listeners for mouse clicked/not
+      gridCell.addEventListener("mousedown", () => {
+        isMouseDown = true;
+      });
+      gridCell.addEventListener("mouseup", () => {
+        isMouseDown = false;
+      });
       gridCell.addEventListener("mouseover", colorChoice);
       gridContainer.appendChild(gridCell);
     }
@@ -40,10 +48,12 @@ function resetGrid() {
 // onclick function to choose color
 let color = "black";
 function colorChoice() {
-  if (color === "random") {
-    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-  } else {
-    this.style.backgroundColor = color;
+  if (isMouseDown) {
+    if (color === "random") {
+      this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+      this.style.backgroundColor = color;
+    }
   }
 }
 
