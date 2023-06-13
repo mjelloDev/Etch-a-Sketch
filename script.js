3d6612b77c62583a4c5439692639db3327538e0c
snappyjel <leviftr1@gmail.com>
2023-06-14 00:11:26 +0700

    Fix typo

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const header = document.querySelector("header");
 const container = document.querySelector(".flex-container");
 
-
 function createGrid() {
   //Determine the size of the canvas
   let gridSize = parseInt(document.getElementById("gridSizeInput").value);
@@ -26,18 +25,44 @@ function createGrid() {
     for (let j = 0; j < gridSize; j++) {
       let gridCell = document.createElement("div");
       gridCell.classList.add("cell");
-      // // Add event listeners for mouse clicked/not
-      // gridCell.addEventListener("mousedown", () => {
-      //   isMouseDown = true;
-      // });
-      // gridCell.addEventListener("mouseup", () => {
-      //   isMouseDown = false;
-      // });
       gridCell.addEventListener("mouseover", colorChoice);
       gridContainer.appendChild(gridCell);
     }
   }
 }
+
+// function createGrid() {
+//   // Determine the size of the canvas
+//   let gridSize = parseInt(document.getElementById("gridSizeInput").value);
+//
+//   // Validate gridSize input value
+//   if (gridSize < 1 || gridSize > 100) {
+//     alert("Please enter a number between 1 and 100");
+//     return;
+//   }
+//
+//   // Clear existing grid
+//   let gridContainer = document.getElementById("gridContainer");
+//   gridContainer.innerHTML = "";
+//
+//   // Set grid styles
+//   gridContainer.style.display = "flex";
+//   gridContainer.style.flexWrap = "wrap";
+//   gridContainer.style.width = "400px"; //
+//
+//   // Calculate the size of each cell based on the grid size
+//   let cellSize = 100 / gridSize;
+//
+//   // Loop to create cells
+//   for (let i = 0; i < gridSize * gridSize; i++) {
+//     let gridCell = document.createElement("div");
+//     gridCell.classList.add("cell");
+//     gridCell.style.width = `calc(${cellSize}% - 2px)`; // Subtracting 2px to account for borders/margins
+//     gridCell.style.height = `calc(${cellSize}% - 2px)`;
+//     gridCell.addEventListener("mouseover", colorChoice);
+//     gridContainer.appendChild(gridCell);
+//   }
+// }
 
 function resetGrid() {
   let gridContainer = document.getElementById("gridContainer");
